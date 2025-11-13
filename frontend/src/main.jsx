c004f944c4f07fee6a9bbc82cb2a1b88657dbf87
Anuj1212G <anujgupta12122004@gmail.com>
2025-11-13 17:37:21 +0530

    link corrected

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,7 +18,7 @@ import AppRoutes from "./routes";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 import "./index.css";
-
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
   
