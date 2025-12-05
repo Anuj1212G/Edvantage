2aaa51593a97bf636d7845266aa42cde8fd014e2
Anuj1212G <anujgupta12122004@gmail.com>
2025-12-06 01:57:48 +0530

    correct the frontend and backend deployement

--- backend/src/app.js
+++ backend/src/app.js
@@ -36,7 +36,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
